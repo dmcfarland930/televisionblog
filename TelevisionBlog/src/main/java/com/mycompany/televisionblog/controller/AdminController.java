c6fe07e7c13a9cfb1eee628df019d4ce99c540de
mikemox <mikemoxx@gmail.com>
2016-06-23 11:40:53 -0400

    Added blog list page and table

--- TelevisionBlog/src/main/java/com/mycompany/televisionblog/controller/AdminController.java
+++ TelevisionBlog/src/main/java/com/mycompany/televisionblog/controller/AdminController.java
@@ -6,7 +6,9 @@
 package com.mycompany.televisionblog.controller;
 
 import com.mycompany.televisionblog.dao.BlogPostDao;
+import com.mycompany.televisionblog.dao.PageDao;
 import com.mycompany.televisionblog.dto.BlogPost;
+import com.mycompany.televisionblog.dto.Page;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -26,10 +28,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class AdminController {
     
     BlogPostDao postDao;
+    PageDao pageDao;
     
     @Inject
-    public AdminController(BlogPostDao postDao) {
+    public AdminController(BlogPostDao postDao, PageDao pageDao) {
         this.postDao = postDao;
+        this.pageDao = pageDao;
     }
     
     @RequestMapping(value="/", method=RequestMethod.GET) 
@@ -38,17 +42,40 @@ public class AdminController {
         List<BlogPost> pendingPosts = postDao.listUnapproved();
         model.put("pendingPosts", pendingPosts);
         return "adminHome";
-    }    
+    } 
     
-    @RequestMapping(value="/{id}", method=RequestMethod.POST)
+    @RequestMapping(value="/post", method=RequestMethod.GET) 
+    public String blogPosts(Map model) {
+        
+        List<BlogPost> posts = postDao.list();
+        model.put("posts", posts);
+        return "post";
+    } 
+    
+    @RequestMapping(value="/page", method=RequestMethod.GET) 
+    public String staticPages(Map model) {
+        
+        List<Page> pages = pageDao.list();
+        model.put("pages", pages);
+        return "page";
+    } 
+    
+    
+    
+    @RequestMapping(value="/approval/{id}/{val}", method=RequestMethod.POST)
     @ResponseBody
-    public void approvePost(@PathVariable("id") Integer id) {
+    public void approvePost(@PathVariable("id") Integer id, @PathVariable("val") Integer val) {
         
         Date date = new Date();
         BlogPost post = postDao.get(id);
         post.setPostDate(date);
         
-        post.setApproved(true);
+        if(val == 1) {
+            post.setApproved(true);
+        } else if(val == 2) {
+            post.setApproved(false);
+        }
+        
         postDao.update(post);
         
     }
--- TelevisionBlog/src/main/webapp/js/adminApp.js
+++ TelevisionBlog/src/main/webapp/js/adminApp.js
@@ -9,7 +9,33 @@ $(document).ready(function() {
         var postId = $(e.target).data("post-id");
        
         $.ajax({
-            url: contextRoot + "/admin/" + postId,
+            url: contextRoot + "/admin/approval/" + postId +"/1",
+            type: "POST",
+            datatype: "json",
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Accept", "application/json");
+                xhr.setRequestHeader("Content-type", "application/json");
+            },
+            success: function(status, data) {
+                
+                $("#post-row-"+postId).remove();
+            },
+            error: function(status, data) {
+               
+            }
+        });
+        
+    });
+    
+    //Approve Pending Posts
+    $(document).on("click", ".disapprove-post-link", function(e) {
+        
+        e.preventDefault();
+        
+        var postId = $(e.target).data("post-id");
+       
+        $.ajax({
+            url: contextRoot + "/admin/approval/" + postId + "/2",
             type: "POST",
             datatype: "json",
             beforeSend: function(xhr) {
