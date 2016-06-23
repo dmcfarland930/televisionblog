ff696899f0053b7e7cde029a4409c0bb21d34653
mikemox <mikemoxx@gmail.com>
2016-06-23 09:47:38 -0400

    Added Admin Controller/Jsp landing page

---
+++ TelevisionBlog/src/main/java/com/mycompany/televisionblog/controller/AdminController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.televisionblog.controller;
+
+import com.mycompany.televisionblog.dao.BlogPostDao;
+import com.mycompany.televisionblog.dto.BlogPost;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Inject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author apprentice
+ */
+@Controller
+@RequestMapping(value="/admin")
+public class AdminController {
+    
+    BlogPostDao postDao;
+    
+    @Inject
+    public AdminController(BlogPostDao postDao) {
+        this.postDao = postDao;
+    }
+    
+    @RequestMapping(value="/", method=RequestMethod.GET) 
+    public String home(Map model) {
+        
+        List<BlogPost> pendingPosts = postDao.listUnapproved();
+        model.put("pendingPosts", pendingPosts);
+        return "adminHome";
+    }    
+    
+}
